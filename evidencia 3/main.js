dd04ab2243169d93d69f763f688728fa955d44f3
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-11-20 17:11:01 -0500

    evidencia 4

--- evidencia 3/main.js
+++ evidencia 3/main.js
@@ -4,28 +4,10 @@ addEventListener("DOMContentLoaded", (e)=>{
     formulario.addEventListener(`submit`,(e)=>{
         e.preventDefault();
         let dato=Object.fromEntries(new FormData(e.target));
-        console.log(dato);
-        precio=Number(dato.precio)
-            if(dato.moneda=="dolares"){
-                respuesta.innerHTML="dolares a pesos = "+ precio*5056.34
-                
-            }
-            else if (dato.moneda=="pesos" ){
-                respuesta.innerHTML="pesos a dolares ="+precio*0.00020
-            }
-        })
+        numero=Number(dato.numero)
+        let fare=(9/5*numero)+32;
+        respuesta.innerHTML=``+fare
 
-
-
-        document.querySelector("#selecionar").addEventListener("change", (e)=>{          
-            if(e.target.value =="dolares"){
-                cambiar.innerHTML = "ingrese dolares a pesos colombianos "
-    
-            }
-            else if(e.target.value =="pesos"){
-                cambiar.innerHTML="ingrese pesos colombianos a dolares"
-    
-            }
         })
 
 
