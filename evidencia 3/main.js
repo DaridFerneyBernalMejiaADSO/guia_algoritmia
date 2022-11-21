b8841549cdf81510502bbdc9287e0a8e20afbd55
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-11-20 19:51:24 -0500

    ejercicio 5

--- evidencia 3/main.js
+++ evidencia 3/main.js
@@ -4,11 +4,14 @@ addEventListener("DOMContentLoaded", (e)=>{
     formulario.addEventListener(`submit`,(e)=>{
         e.preventDefault();
         let dato=Object.fromEntries(new FormData(e.target));
-        numero=Number(dato.numero)
-        let fare=(9/5*numero)+32;
-        respuesta.innerHTML=``+fare
-
-        })
+        hora=Number(dato.hora)
+        direccion=dato.direccion
+        if (hora>=5)
+        respuesta.innerHTML=`ya es hora de ir al trabajo son las ${hora} la direcion es : ${direccion} `
+        else{
+            respuesta.innerHTML=`aun puedes seguir durmiendo`
+        }
+    })
 
 
     
