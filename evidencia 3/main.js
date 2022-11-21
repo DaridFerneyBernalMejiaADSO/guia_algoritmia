559e4058489eed87f9675bd52317f88a6de62411
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-11-20 21:38:32 -0500

    evidencia 6

--- evidencia 3/main.js
+++ evidencia 3/main.js
@@ -1,16 +1,27 @@
 addEventListener("DOMContentLoaded", (e)=>{
     let formulario=document.querySelector("#formulario")
-
+     cont=1;
+    
     formulario.addEventListener(`submit`,(e)=>{
         e.preventDefault();
         let dato=Object.fromEntries(new FormData(e.target));
-        hora=Number(dato.hora)
-        direccion=dato.direccion
-        if (hora>=5)
-        respuesta.innerHTML=`ya es hora de ir al trabajo son las ${hora} la direcion es : ${direccion} `
-        else{
-            respuesta.innerHTML=`aun puedes seguir durmiendo`
-        }
+        let imple=dato.imple
+        let tabla =document.querySelector("tbody");
+        tabla.insertAdjacentHTML("beforeend",`
+             <tr>
+                 <td>${cont}</td>
+                 <td>${imple}</td>
+              </tr>
+        `)
+        cont++
+        
+        
+        
+        
+        
+        
+
+
     })
 
 
